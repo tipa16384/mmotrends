c2ad30be284d5da1d59952affc1f5406182f83d6
Brenda Holloway <brendahol@gmail.com>
2021-08-12 23:12:25 -0400

    Sort with quick sort.

--- mmotrends.py
+++ mmotrends.py
@@ -3,6 +3,7 @@
 from pytrends.request import TrendReq
 from time import time, sleep
 from random import random, randint
+from json import dumps, loads
 
 # name of the file containing our MMO names
 namefile = 'names.txt'
@@ -31,25 +32,8 @@ timeframe = 'today 3-m'
 with open(namefile, 'r') as f:
     mmolist = f.read().split('\n')
 
-# hack to replace standard comparison function with one I define
-def cmp_to_key(mycmp):
-    'Convert a cmp= function into a key= function'
-    class K:
-        def __init__(self, obj, *args):
-            self.obj = obj
-        def __lt__(self, other):
-            return mycmp(self.obj, other.obj) < 0
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
-        def __eq__(self, other):
-            return mycmp(self.obj, other.obj) == 0
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
-    return K
+# Information to improve the search, keyed by game name, with information from suggestions API.
+mmokwds = {}
 
 def gaugeInterest(gamedata):
     """
@@ -62,51 +46,88 @@ def gaugeInterest(gamedata):
         sum = sum + gamedata[i]
     return sum / numrows
 
-def compareInterestOverTime(gamea, gameb):
+def gimme_some_chunks(list):
+    'generator to yield four entry chunks from a list'
+    for y in range(0, len(list), 4):
+        yield list[y:(y+4)]
+
+def compare_games_by_trends(pivot, list):
+    'Breaks the game list into four game chunks and sorts them'
+    lt = []
+    ge = []
+    for chunk in gimme_some_chunks(list):
+        (nlt, nge) = compare_chunks_by_trends(pivot, chunk)
+        lt = lt + nlt
+        ge = ge + nge
+    return (lt, ge)
+
+def compare_chunks_by_trends(pivot, list):
     """
-    Compare the relative interest between two games.
-    1. Google Trends allows only a small number of calls per some time period. If we
-       make just one call a minute, we're fine, so throttle our requests to that.
-    2. Pass the games to Google Trends. It returns a dictionary keyed off the game
-       name containing a list of dictionaries keyed off date. The value for each
-       entry is the interest for that day.
-    3. The return value is the average interest for game B less the average interest
-       for game A. This sorts the game with the greater interest first.
-    If there is an error, print the error and just return the interest in the two
-    games as equal. The error seems to be if one game name is blank, or we have overrun
-    our quota and Google Trends is through with us for a minute.
+    calls Trend API with up to four games, and the pivot. Returns list of games
+    that are ranked lower and higher.
     """
     global lastCallTime, requestSpacing, spacingDuration
+    payload = [pivot] + list
     try:
         now = time()
         waittime = now - lastCallTime
         lastCallTime = now
         if waittime < requestSpacing:
             interval = requestSpacing - waittime + 1
-            print ('Games={}, {} -- Sleeping for {} seconds'.format(gamea, gameb, interval))
+            print ('Sleeping for {} seconds'.format(interval))
             sleep(interval)
-            if False and (random() > 0.95):
-                print ('Dropping the sleep and taking our chances')
-                requestSpacing = 0
 
-        pytrends.build_payload([gamea, gameb], cat=mmocat, timeframe=timeframe, geo='', gprop='')
+        pytrends.build_payload(payload, cat=mmocat, timeframe=timeframe, geo='', gprop='')
+
+        # Manipulate pytrends internal variables to add more parameters to our
+        # search terms, based on suggestions.
+        reqdict = loads(pytrends.token_payload['req'])
+        for compitem in reqdict['comparisonItem']:
+            if compitem['keyword'] in mmokwds:
+                kwd = mmokwds[compitem['keyword']]
+                compitem['keyword'] = kwd['keyword']
+                compitem['title'] = kwd['title']
+                compitem['name'] = kwd['title']
+                compitem['type'] = kwd['type']
+        
+        pytrends.token_payload['req'] = dumps(reqdict)
+
         data = pytrends.interest_over_time()
-        gameAInterest = gaugeInterest(data[gamea]) if gamea in data else 0
-        gameBInterest = gaugeInterest(data[gameb]) if gameb in data else 0
-        return gameBInterest - gameAInterest
+
+        # build a dictionary keyed by game name with value the average interest
+        # over the past 90 days.
+        game_trend_table = {}
+
+        for g in payload:
+            game_trend_table[g] = gaugeInterest(data[g]) if g in data else 0
+
+        print (game_trend_table)
+
+        lt = []
+        ge = []
+
+        # make the LT and GE lists and return them.
+        for g in list:
+            lt.append(g) if game_trend_table[g] > game_trend_table[pivot] else ge.append(g)
+
+        return (lt, ge)
     except Exception as err:
         print ('Error was {}'.format(err))
         requestSpacing = spacingDuration
-        print ('Died with error. Games are {}, {}. Spacing set to {}'.format(gamea, gameb, requestSpacing))
+        print ('Died with error. Games are {}, {}. Spacing set to {}'.format(pivot, list, requestSpacing))
         sleep(requestSpacing)
-        return compareInterestOverTime(gamea, gameb)
-
-def lengthSort(gamea, gameb):
-    return len(gameb) - len(gamea)
+        return compare_chunks_by_trends(pivot, list)
+     
+def qsort(list):
+    'implementation of the qsort algorithm'
+    if len(list) <= 1: return list
+    pivot = list[0]
+    (lt, ge) = compare_games_by_trends(pivot, list[1:])
+    return qsort(lt) + [pivot] + qsort(ge)
 
 def sortGames():
     'Sort the game list by relative interest'
-    return sorted(mmolist, key=cmp_to_key(lengthSort))
+    return qsort(mmolist)
 
 def rankAndWrite():
     'call sortGames to sort the games, then write the results to a file'
@@ -116,6 +137,23 @@ def rankAndWrite():
         for game in l:
             print('{}. {}'.format(l.index(game)+1, game), file=f)
 
+def build_kwds():
+    'Uses the Suggestions API to try and regularize the game names'
+    for g in mmolist:
+        sleep(requestSpacing)
+        sugs = pytrends.suggestions(g)
+        mmokwds[g] = { 'keyword': g, 'title': g, 'type': 'Search term' }
+        for sug in sugs:
+            # 2010 video game seems to be a category for just FFXIV
+            if sug['type'] == '2010 video game' or sug['type'] == 'Online game' or sug['type'] == 'Video game':
+                mmokwds[g]['keyword'] = sug['mid']
+                mmokwds[g]['type'] = sug['type']
+                mmokwds[g]['title'] = sug['title']
+                break
+        print(mmokwds[g])
+
+
 # if calling as main, immediately call rankAndWrite to do the ranking.
 if __name__ == "__main__":
+    build_kwds()
     rankAndWrite()
